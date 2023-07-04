4eaf21606f156c3aa9476f2646d43cf5589fa1bd
”Kirill <kirillgrekhovodov@gmail.com>
2023-07-04 20:09:18 +0600

    Добавил модель комментария и модель тега

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
 
-class Article(models.Model):
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    class Meta:
+        abstract = True
+
+
+class Article(AbstractModel):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Название")
     author = models.CharField(max_length=50, null=False, blank=False, verbose_name="Автор", default="Неизвестный")
     content = models.TextField(max_length=2000, verbose_name="Контент")
+    tags = models.ManyToManyField('webapp.Tag', related_name='articles', through='webapp.ArticleTag',
+                                  through_fields=('article', 'tag'), blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    # tags = models.ManyToManyField('webapp.Tag', related_name='articles', blank=True)
+
+
 
     def __str__(self):
         return f"{self.pk} {self.title}: {self.author}"
@@ -16,3 +27,36 @@ class Article(models.Model):
         db_table = "articles"
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
+
+
+class Comment(AbstractModel):
+    article = models.ForeignKey('webapp.Article', related_name='comments', on_delete=models.CASCADE,
+                                verbose_name='Статья')
+    text = models.TextField(max_length=400, verbose_name='Комментарий')
+    author = models.CharField(max_length=40, null=True, blank=True, default='Аноним', verbose_name='Автор')
+
+    def __str__(self):
+        return self.text[:20]
+
+
+class Tag(AbstractModel):
+    name = models.CharField(max_length=31, verbose_name='Тег')
+    # articles = models.ManyToManyField('webapp.Article', related_name='tags', through='webapp.ArticleTag',
+    #                                   through_fields=('tag', 'article'), blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "tags"
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+
+class ArticleTag(AbstractModel):
+    article = models.ForeignKey('webapp.Article', related_name='article_tags', on_delete=models.CASCADE,
+                                verbose_name='Статья')
+    tag = models.ForeignKey('webapp.Tag', related_name='tag_articles', on_delete=models.CASCADE, verbose_name='Тег')
+
+    def __str__(self):
+        return "{} | {}".format(self.article, self.tag)
