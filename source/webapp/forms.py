3687ecdc52562d69b1b8575898e7b9a3e07660d2
”Kirill <kirillgrekhovodov@gmail.com>
2023-07-05 20:39:11 +0600

    добавили в форму классы будстрапа

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,6 +5,13 @@ from webapp.models import Tag
 
 
 class ArticleForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for v in self.visible_fields():
+            v.field.widget.attrs["class"] = "form-control"
+
     title = forms.CharField(max_length=50, required=True, label="Название")
     author = forms.CharField(max_length=50, required=True, label="Автор")
     content = forms.CharField(max_length=2000, required=True, label="Контент",
